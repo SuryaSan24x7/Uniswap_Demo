2c767176e5bfa8c552846dff393c123c994cf898
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-01-08 01:08:18 +0530

    Hardhat Setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Uniswap_Demo
+1. Install HardHat npm install --save-dev hardhat. 
+2. To create the sample project, run npx hardhat init in the Project File.
+3. npx hardhat compile , To Compile The Smart contract.
+4. npx hardhat test , To Test the Smart contracts
+5. npx hardhat run scripts/deploy.js --network localhost , To Deploy the Smart Contract
+6. By default, Hardhat will spin up a new in-memory instance of Hardhat Network on startup. It's also possible to run Hardhat Network in a standalone fashion so that external clients can connect to it. This could be a wallet, your Dapp front-end, or a script.
+
+To run Hardhat Network in this way, run npx hardhat node.
---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.19",
+// };
+module.exports = {
+  // defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+    },
+    dev: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+};
